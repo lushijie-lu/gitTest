b7244983cf371bdf191dc7345071ff38176f5477
lushijie-lu <jack_lusj@163.com>
2020-04-05 18:22:29 +0800

    第一次提交

---
+++ InetSocketAddressDemo.java
@@ -0,0 +1,17 @@
+package cn.tedu.net.address;
+
+import java.net.InetSocketAddress;
+
+public class InetSocketAddressDemo {
+    public static void main(String[] args) {
+        //创建代表IP地址和端口的类的对象
+        InetSocketAddress inetSocketAddress=new InetSocketAddress
+                ("localhost",8090);
+        //localhost映射成的IP地址127.0.0.1
+        //127.0.0.1永远指向本地
+        //返回IP地址、主机名（计算机名称）、端口号
+        System.out.println(inetSocketAddress.getAddress());
+        System.out.println(inetSocketAddress.getHostName());
+        System.out.println(inetSocketAddress.getPort());
+    }
+}
